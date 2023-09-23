480a95f025391678c504190004e908e65673c64e
rishabh <rishabh5sing@gmail.com>
2023-09-23 22:26:53 +0530

    test

--- src/test/java/WalmartCaTest/OnboardTest.java
+++ src/test/java/WalmartCaTest/OnboardTest.java
@@ -158,7 +158,7 @@ public class OnboardTest extends Baseclass {
 			Assert.assertTrue(false, "ProductVendor options are not showing");
 		}
 	}
-	@Test(priority = 16)
+	@Test(priority = 16)// This is 
 	public void selectCollection() throws InterruptedException {
 		ArrayList<WebElement> AllOptions = obj2.selectCollection();
 		if(AllOptions.size() == 0) {
